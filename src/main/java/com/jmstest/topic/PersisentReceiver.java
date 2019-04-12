05086057cf2a402f2d747f525276c88989141a09
qianwx1993 <zhouxiao0629@qq.com>
2019-04-12 17:19:16 +0800

    创建订阅模式，持久化订阅实例

---
+++ src/main/java/com/jmstest/topic/PersisentReceiver.java
@@ -0,0 +1,38 @@
+package com.jmstest.topic;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.RedeliveryPolicy;
+
+import javax.jms.*;
+
+public class PersisentReceiver {
+	//TODO 持久化订阅消费这
+	public static void main(String[] args) throws Exception {
+		ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory("tcp://127.0.0.1:61616");
+		
+		RedeliveryPolicy policy = new RedeliveryPolicy();
+		policy.setMaximumRedeliveries(3);
+		cf.setRedeliveryPolicy(policy);
+		
+		Connection connection = cf.createConnection();
+		connection.start();
+
+		final Session session = connection.createSession(Boolean.TRUE,
+				Session.AUTO_ACKNOWLEDGE);
+		Destination destination = session.createTopic("myTopic");
+
+		MessageConsumer consumer = session.createConsumer(destination);
+
+
+		Message message = consumer.receive();
+		while(message!=null) {
+			TextMessage txtMsg = (TextMessage)message;
+			System.out.println("收到消息：" + txtMsg.getText());
+			message = consumer.receive(1000L);
+		}
+
+		session.close();
+		connection.close();
+	}
+
+}
---
+++ src/main/java/com/jmstest/topic/PersisentSender.java
@@ -0,0 +1,48 @@
+package com.jmstest.topic;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+public class PersisentSender {
+	/**
+	 * 持久化订阅发送者
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://127.0.0.1:61616");
+		Session session=null;
+		Connection connection=null;
+		try {
+			connection = connectionFactory.createConnection();
+
+			session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
+			Destination destination = session.createTopic("myTopic");
+
+			MessageProducer producer = session.createProducer(destination);
+
+			producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+			//必须设置了投递模式后，启动connection才有效
+			connection.start();
+
+			for (int i=0;i<3;i++){
+				TextMessage textMessage = session.createTextMessage("发送消息111" + i);
+				System.out.println(textMessage.getText());
+				producer.send(textMessage);
+
+			}
+			session.commit();
+
+		}catch (Exception e){
+			e.printStackTrace();
+		}finally {
+			try {
+				session.close();
+				connection.close();
+			} catch (JMSException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
